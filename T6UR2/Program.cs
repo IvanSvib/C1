9fc54917c59a0082f9931635ac57f3c92fb0047f
IvanSvib <164069495+IvanSvib@users.noreply.github.com>
2024-03-28 20:30:36 +0300

    Добавьте файлы проекта.

---
+++ T6UR2/Program.cs
@@ -0,0 +1,24 @@
+﻿////Parse
+string number = $"{28}";
+byte number1 = byte.Parse(number);
+Console.WriteLine(number1);
+
+string name = $"{-454}";
+int name1 = int.Parse(name);
+Console.WriteLine(name1);
+
+
+//ToString()
+double x1 = 777;
+Console.WriteLine((x3).ToString());
+
+double z3 = 444;
+Console.WriteLine((z3).ToString());
+
+
+////GetType
+short b = (short)2;
+Console.WriteLine(b.ToString());
+
+int i = (int)b;
+Console.WriteLine(i.GetType());
