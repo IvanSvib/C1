7072e592e54744cc3c95647f4d7688ba14bd3f5a
IvanSvib <164069495+IvanSvib@users.noreply.github.com>
2024-04-07 20:18:48 +0300

    T10U1

---
+++ T10U1/Program.cs
@@ -0,0 +1,21 @@
+﻿
+//Тема 10. урок 1
+Console.WriteLine("Введите радиус(R) окружности:");
+double R = double.Parse(Console.ReadLine()!);
+double L = 2 * Math.PI * R;
+double S = Math.PI * R * R;
+double S1 = Math.Round(S);
+double L1 = Math.Round(L);
+Console.Write($"Длина окружности = {L1:F2}" +
+    $"\n Площадь окружности равна {S1:F2}" +
+    $"\n");
+
+//Тема 1. Урок 2
+
+Console.WriteLine("Введите значение катета a треугольника:");
+double a = double.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите значение гипотенузы треугольника:");
+double c = double.Parse(Console.ReadLine()!);
+double b = c * (Math.Cos(a));
+Console.Write($"Значение катета b = {b:F2}.");
+
